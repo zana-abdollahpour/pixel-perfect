b488626f8e6bbfe966cf80e98544afdc40b9011a
zana-abdollahpour <zana.ap4@gmail.com>
2025-04-10 14:30:08 +0330

    finish responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { EpicStackLogo, logos } from "@/logos/logos";
 export default function App() {
   return (
     <div className="grid min-h-screen place-items-center px-4 py-16">
-      <div className="grid place-items-center gap-12">
-        <div className="flex max-w-md flex-col items-center text-center">
+      <div className="grid place-items-center gap-12 sm:gap-16 xl:grid-cols-[auto_1fr] xl:gap-24">
+        <div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-start xl:text-left">
           <EpicStackLogo className="size-20" />
-          <h1 className="mt-6 text-4xl font-medium">
+          <h1 className="sm:text-4.5xl lg:text-5.5xl mt-6 text-4xl font-medium md:mt-8 md:text-5xl">
             The <span className="text-highlight">Epic</span> Stack
           </h1>
-          <p className="mt-4 text-slate-600">
+          <p className="mt-4 text-slate-600 sm:text-lg md:mt-6 md:text-xl">
             Check the{" "}
             <a className="text-black underline" href="#">
               Getting Started
@@ -17,12 +17,12 @@ export default function App() {
             guide file for how to get your project off the ground!
           </p>
         </div>
-        <ul className="flex max-w-3xl flex-wrap justify-center gap-2">
+        <ul className="flex max-w-3xl flex-wrap justify-center gap-2 sm:gap-4 xl:grid xl:grid-flow-col xl:grid-cols-5 xl:grid-rows-6">
           {logos.map((logo) => (
             <li key={logo.href}>
               <a
                 href={logo.href}
-                className="bg-highlight/[7%] grid size-20 place-items-center rounded-2xl p-4"
+                className="bg-highlight/[7%] grid size-20 place-items-center rounded-2xl p-4 sm:size-24"
               >
                 <img src={logo.src} alt={logo.alt} />
               </a>
